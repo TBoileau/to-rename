6d0c5068197d37b62f963635e8571e4fae4e641f
TBoileau <t-boileau@email.com>
2022-07-14 19:23:09 +0200

    build: install some dependencies for analyse and fixtures
    * friendsofphp/php-cs-fixer
    * dama/doctrine-test-bundle
    * orm-fixtures
    * phpstan/phpstan
    * phpstan/phpstan-deprecation-rules
    * phpstan/phpstan-doctrine
    * phpstan/phpstan-phpunit
    * phpstan/phpstan-strict-rules
    * phpstan/phpstan-symfony

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
