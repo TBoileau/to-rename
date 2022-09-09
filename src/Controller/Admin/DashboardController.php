6b26508a094db29506e9ef590e5707902f7d9445
Thomas Boileau <t-boileau@email.com>
2022-09-09 10:48:02 +0200

    Videos et logos (#11)
    * build: installation de easy admin
    
    * feat (admin): création du tableau de bord
    
    * feat (admin): gestion des vidéos (#9)
    
    * feat (admin): lier une vidéo à un live
    
    * feat (admin): gestion des logos (#10)

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Entity\Logo;
+use App\Entity\Video;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Twitch');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Videos', 'fab fa-youtube', Video::class);
+        yield MenuItem::linkToCrud('Logos', 'fa fa-image', Logo::class);
+    }
+}
---
+++ src/Entity/Video.php
@@ -0,0 +1,130 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\VideoRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Url;
+
+#[Entity(repositoryClass: VideoRepository::class)]
+class Video
+{
+    #[Id]
+    #[GeneratedValue]
+    #[Column(type: Types::INTEGER)]
+    private ?int $id = null;
+
+    #[NotBlank]
+    #[Column(type: Types::STRING)]
+    private string $title;
+
+    #[NotBlank]
+    #[GreaterThan(0)]
+    #[Column(type: Types::INTEGER)]
+    private int $season;
+
+    #[NotBlank]
+    #[GreaterThan(0)]
+    #[Column(type: Types::INTEGER)]
+    private int $episode;
+
+    #[ManyToOne(targetEntity: Logo::class)]
+    #[JoinColumn(nullable: false)]
+    private Logo $logo;
+
+    #[Column(type: Types::STRING)]
+    private string $thumbnail;
+
+    #[Url]
+    #[NotBlank]
+    #[Column(type: Types::STRING)]
+    private string $link;
+
+    #[ManyToOne(targetEntity: Live::class)]
+    private ?Live $live;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getSeason(): int
+    {
+        return $this->season;
+    }
+
+    public function setSeason(int $season): void
+    {
+        $this->season = $season;
+    }
+
+    public function getEpisode(): int
+    {
+        return $this->episode;
+    }
+
+    public function setEpisode(int $episode): void
+    {
+        $this->episode = $episode;
+    }
+
+    public function getThumbnail(): string
+    {
+        return $this->thumbnail;
+    }
+
+    public function setThumbnail(string $thumbnail): void
+    {
+        $this->thumbnail = $thumbnail;
+    }
+
+    public function getLink(): string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): void
+    {
+        $this->link = $link;
+    }
+
+    public function getLive(): ?Live
+    {
+        return $this->live;
+    }
+
+    public function setLive(?Live $live): void
+    {
+        $this->live = $live;
+    }
+
+    public function getLogo(): Logo
+    {
+        return $this->logo;
+    }
+
+    public function setLogo(Logo $logo): void
+    {
+        $this->logo = $logo;
+    }
+}
