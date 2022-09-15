7a9b95a211f2e021410d76c245d8a05e4f088ea5
TBoileau <t-boileau@email.com>
2022-09-15 18:50:54 +0200

    feat (live): tweet un live (#37)

--- src/Controller/Admin/LiveCrudController.php
+++ src/Controller/Admin/LiveCrudController.php
@@ -5,16 +5,52 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Entity\Live;
+use App\OAuth\Api\Twitter\TwitterClient;
+use App\OAuth\Security\Token\OAuthToken;
+use App\OAuth\Security\Token\TokenStorageInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class LiveCrudController extends AbstractCrudController
 {
+    public function __construct(private TokenStorageInterface $tokenStorage)
+    {
+    }
+
     public static function getEntityFqcn(): string
     {
         return Live::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->setDefaultSort(['livedAt' => 'DESC']);
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        /** @var OAuthToken $twitterToken */
+        $twitterToken = $this->tokenStorage['twitter'];
+
+        if (!$twitterToken->isAuthenticated()) {
+            $actions->disable('tweet');
+        }
+
+        $tweet = Action::new('tweet', 'Tweet')
+            ->linkToRoute('admin_live_tweet', static fn (Live $live): array => ['id' => $live->getId()]);
+
+        return $actions
+            ->add(Crud::PAGE_INDEX, $tweet)
+            ->add(Crud::PAGE_DETAIL, $tweet)
+            ->add(Crud::PAGE_INDEX, Action::DETAIL);
     }
 
     public function configureFields(string $pageName): iterable
@@ -23,5 +59,28 @@ final class LiveCrudController extends AbstractCrudController
         yield AssociationField::new('planning', 'Planning');
         yield DateTimeField::new('livedAt', 'Date')
             ->setFormat('dd/MM/yyyy HH:mm');
+    }
+
+    #[Route('/admin/lives/{id}/tweet', name: 'admin_lives_tweet')]
+    public function tweet(Live $live, AdminUrlGenerator $adminUrlGenerator, TwitterClient $twitterClient): RedirectResponse
+    {
+        $twitterClient->tweet(<<<EOF
+Le live Twitch commence à {$live->getLivedAt()->format('H:i')} !
+
+{$live->getDescription()} 
+
+{$live->getDescription()}
+
+https://twitch.tv/toham
+EOF
+        );
+
+        return new RedirectResponse(
+            $adminUrlGenerator
+                ->setController(self::class)
+                ->setAction(Action::DETAIL)
+                ->setEntityId($live->getId())
+                ->generateUrl()
+        );
     }
 }
