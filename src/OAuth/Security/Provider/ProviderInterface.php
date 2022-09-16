06e45601cd54a0d3f55d0332e1ca2595d6798f49
Thomas Boileau <t-boileau@email.com>
2022-09-16 11:16:29 +0200

    Refresh token (#44)
    * feat (oauth): gestion du refresh token de google (#42)
    
    * feat (oauth): gestion du refresh token de twitter (#43)

--- src/OAuth/Security/Provider/ProviderInterface.php
+++ src/OAuth/Security/Provider/ProviderInterface.php
@@ -12,4 +12,9 @@ interface ProviderInterface
      * @return array<string, mixed>
      */
     public function fetchAccessToken(Request $request): array;
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function fetchAccessTokenWithRefreshToken(string $refreshToken): array;
 }
